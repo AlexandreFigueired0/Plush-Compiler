696f57c7bc57879673f68f1422fb1ca96b1d0369
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2024-05-31 18:35:43 +0100

    Added project structure explanation in readme

--- utils.pl
+++
@@ -1,9 +0,0 @@
-from utils2 import *
-
-function test() : int {
-    test := 1;
-}
-
-function hello(){
-    hello_world();
-}
--- utils2.pl
+++
@@ -1,7 +0,0 @@
-function hello_world(){
-    print_string("Hello, World!\n");
-}
-
-function test1() : int {
-    test1 := 1;
-}
